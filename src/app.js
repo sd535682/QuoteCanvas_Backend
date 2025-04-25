59f32c70921d6eff36739faa61314926157897af
sd535682 <sd535682@gmail.com>
2025-04-25 16:11:38 +0530

    initial setup and dependencies

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+export default app;
