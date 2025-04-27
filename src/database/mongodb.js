997bca6e753ee1e6d0a69698d750303d7ff24f0e
sd535682 <sd535682@gmail.com>
2025-04-27 13:41:35 +0530

    user model

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -2,15 +2,15 @@ import mongoose from "mongoose";
 import { MONGODB_URI } from "../config/env.js";
 
 if (!MONGODB_URI) {
-    throw new Error("Please provide MONGODB_URI in the environment variables");
+  throw new Error("Please provide MONGODB_URI in the environment variables");
 }
 
 export const connectMongoDB = async () => {
-    try {
-        await mongoose.connect(MONGODB_URI);
-        console.log("MongoDB connected");
-    } catch (error) {
-        console.error("MongoDB connection error", error);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error", error);
+    process.exit(1);
+  }
 };
