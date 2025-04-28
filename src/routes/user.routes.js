63f40e8f62bf4899e09218d1a99d6b9e946435e1
sd535682 <sd535682@gmail.com>
2025-04-28 22:02:58 +0530

    use auth middleware before making req

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { getUser, getUsers } from "../controllers/user.controller.js";
+import { authorize } from "../middlewares/auth.middleware.js";
 
 // Declare Router
 const userRouter = Router();
 
 // GET all users
-userRouter.get("/", getUsers);
+userRouter.get("/", authorize, getUsers);
 
 // GET user by ID
-userRouter.get("/:id", getUser);
+userRouter.get("/:id", authorize, getUser);
 
 // POST new user
 userRouter.post('/', (req, res) => res.send({ message: 'POST new user' }));
