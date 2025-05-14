e1440416ad27725580a8045b473ed8e8a6caae27
sd535682 <sd535682@gmail.com>
2025-05-14 18:56:22 +0530

    get all public quotes route

--- src/routes/quotes.routes.js
+++ src/routes/quotes.routes.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
 import { authorize } from "../middlewares/auth.middleware.js";
-import { getAllQuotes, getQuoteById, createQuote, updateQuote, deleteQuote } from "../controllers/quotes.controller.js";
+import {
+  getAllQuotes,
+  getQuoteById,
+  createQuote,
+  updateQuote,
+  deleteQuote,
+  getAllPublicQuotes,
+} from "../controllers/quotes.controller.js";
 
 const quoteRouter = Router();
 
 quoteRouter.get("/", authorize, getAllQuotes);
+
+quoteRouter.get("/all", getAllPublicQuotes);
 
 quoteRouter.get("/:id", authorize, getQuoteById);
 
